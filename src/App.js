e5ee9c52b80afad2daa3763ff7dc74ee8a279c09
Jakub <jakuboleszczuk@protonmail.com>
2021-12-16 21:30:31 +0100

    todo: style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import cat from './cat.jpeg';
 
+
+
+const StyledLink = styled(Link)`
+  color: white;
+  underlined: none;
+  text-decoration: none;
+  justify-content: space-around;
+
+  background-color: #696868;
+  padding: 14px;
+  margin-top:10px;
+`
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const A = styled.a`
+  color: white;
+  underlined: none;
+  text-decoration: none;
+  justify-content: space-around;
+
+  background-color: #696868;
+  padding: 14px;
+  margin-top:10px;
+`
+const NavBar = styled.nav`
+  background-color: red;
+  align-items: center;
+`
+function Home() {
+  return (
+    <>
+      <NavBar>
+       <StyledLink  to="/about">About</StyledLink>
+       <A href="https://twitter.com/DrMayPrey">Twitter</A>
+       <A href="https://www.linkedin.com/in/jakub-oleszczuk-25750316b/?locale=en_US">LinkedIn</A>
+      </NavBar>
+
+      <Main>
+        <p>Here Will be everyday one article, for now have a cat because page is in build</p>
+        <img src={cat} alt="Cat"/>
+
+      </Main>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      
+      <NavBar>
+        <StyledLink to="/">Home</StyledLink>
+        <A href="https://twitter.com/DrMayPrey">Twitter</A>
+        <A href="https://www.linkedin.com/in/jakub-oleszczuk-25750316b/?locale=en_US">LinkedIn</A>
+      </NavBar>
+      <Main>
+        <h2>Im Jakub, I'm Web Developer by hobby</h2>
+        <p>
+          I'm 19 y/o, student of Engineering in Centre of Vocational and Continuing Education in Poland
+        </p>
+      </Main>
+    </>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+      <h1>Jacob's blog</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </Main>
   );
 }
 
